d167f95d85ec226de5d7b9242e3248fb4aed55e1
Owain Lewis <owain@owainlewis.com>
2017-04-26 18:14:57 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject akka-http-clj "0.1.0-SNAPSHOT"
+(defproject io.forward/akka-http-clj "0.1.0-SNAPSHOT"
   :description "A high performance HTTP client using Akka"
   :url "http://github.com/owainlewis/akka-http-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.typesafe.akka/akka-actor_2.11 "2.4.8"]
-                 [com.typesafe.akka/akka-stream_2.11 "2.4.8"]
-                 [com.typesafe.akka/akka-http-experimental_2.11 "2.4.8"]
-                 [com.typesafe.akka/akka-http-core_2.11 "2.4.8"]])
+                 [com.typesafe.akka/akka-actor_2.11 "2.5.0"]
+                 [com.typesafe.akka/akka-stream_2.11 "2.5.0"]
+                 [com.typesafe.akka/akka-http-experimental_2.11 "2.5.0"]
+                 [com.typesafe.akka/akka-http-core_2.11 "2.5.0"]])
